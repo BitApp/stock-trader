94c112fe2cde7d1a8518b71b2692ed529c52b52b
AlanGuo <AlanGuo>
2026-03-13 21:28:36 +0800

    fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "stock-tradebot-watch",
-      cwd: "/Users/alanguo/Projects/stock-trader",
+      cwd: "~/projects/stock-trader",
       script: "./scripts/run-watch.sh",
       interpreter: "bash",
       autorestart: true,
